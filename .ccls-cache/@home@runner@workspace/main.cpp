2f2d09cf69fc1289f6ec8228fd7c5f89c38dee9d
tobam <tobamohaqqeq84@gmail.com>
2025-10-19 22:14:20 +0000

    4th

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
-class DoublyLinkedList {
+
+class DoublyLinkedList { 
 private:
 
 struct Node {
@@ -29,94 +30,18 @@ Node* tail;
 public:
 DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
-void insert_after(string value, int position) {
-if (position < 0) {
-cout << "Position must be >= 0." << endl;
-return;
-}
-Node* newNode = new Node(value);
-if (!head) {
-head = tail = newNode;
-return;
-}
-Node* temp = head;
-for (int i = 0; i < position && temp; ++i)
-temp = temp->next;
-if (!temp) {
-cout << "Position exceeds list size. Node not inserted.\n";
-delete newNode;
-return;
-}
-newNode->next = temp->next;
-newNode->prev = temp;
-if (temp->next)
-temp->next->prev = newNode;
-else
-tail = newNode;
-temp->next = newNode;
+void push_back(string v){
+  Node* newNode = new Node(v);
+  if (!tail)
+    head = tail = newNode;
+  else {
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+  }
 }
 
-void delete_val(int value) {
-if (!head) return;
-Node* temp = head;
-while (temp && temp->data != value)
-temp = temp->next;
-if (!temp) return;
-if (temp->prev)
-temp->prev->next = temp->next;
-else
-head = temp->next;
-if (temp->next)
-temp->next->prev = temp->prev;
-else
-tail = temp->prev;
-delete temp;
-}
-
-void delete_pos(int pos) {
-if (!head) {
-cout << "List is empty." << endl;
-return;
-}
-if (pos == 1) {
-pop_front();
-return;
-}
-Node* temp = head;
-for (int i = 1; i < pos; i++){
-if (!temp) {
-cout << "Position doesn't exist." << endl;
-return;
-}
-else
-temp = temp->next;
-}
-if (!temp) {
-cout << "Position doesn't exist." << endl;
-return;
-}
-if (!temp->next) {
-pop_back();
-return;
-}
-  
-Node* tempPrev = temp->prev;
-tempPrev->next = temp->next;
-temp->next->prev = tempPrev;
-delete temp;
-}
-
-void push_back(int v) {
-Node* newNode = new Node(v);
-if (!tail)
-head = tail = newNode;
-else {
-tail->next = newNode;
-newNode->prev = tail;
-tail = newNode;
-}
-}
-void push_front(int v) {
+void push_front(string v) {
 Node* newNode = new Node(v);
 if (!head)
 head = tail = newNode;
@@ -128,10 +53,7 @@ head = newNode;
 }
 
 void pop_front() {
-if (!head) {
-cout << "List is empty." << endl;
-return;
-}
+if (!head) return;
 Node * temp = head;
 if (head->next) {
 head = head->next;
@@ -143,10 +65,7 @@ delete temp;
 }
 
 void pop_back() {
-if (!tail) {
-cout << "List is empty." << endl;
-return;
-}
+if (!tail) return;
 Node * temp = tail;
 if (tail->prev) {
 tail = tail->prev;
